520c163ceb6937022318b8786f753e8aca80396a
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-02-25 20:13:57 +0200

    loadmakers, proxy coordinators

--- pkg/loadmaker/loadmaker.go
+++ pkg/loadmaker/loadmaker.go
@@ -1,6 +1,9 @@
 package loadmaker
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -10,25 +13,53 @@ const (
 )
 
 type ProxyCoordinator struct {
-	LoadMakers []LoadMaker
+	LoadMakers []*LoadMaker
 	TargetTime time.Time
 	Client     http.Client
 }
 
 type LoadMaker struct {
-	Request  http.Request
 	ProxyUrl string // with port if IP
 	Endpoint string
+	Request  *http.Request
 }
 
-func (pc *ProxyCoordinator) New(proxyUrl string, endpoint string, method string, body any) *LoadMaker {
-	lm := LoadMaker{
-		ProxyUrl: proxyUrl,
-		Endpoint: endpoint,
-		Request:  http.NewRequest(method, proxyUrl),
+func NewProxyCoordinator(d time.Duration, opts ...ProxyCoordinatorOption) *ProxyCoordinator {
+	pc := &ProxyCoordinator{
+		LoadMakers: nil,
+		TargetTime: time.Now().Add(d),
+		Client:     http.Client{}, // Default client unless otherwise specified via WithClient option
+	}
+	for _, opt := range opts {
+		opt(pc)
+	}
+	return pc
+}
+
+type ProxyCoordinatorOption func(pc *ProxyCoordinator)
+
+func WithClient(client http.Client) ProxyCoordinatorOption {
+	return func(pc *ProxyCoordinator) {
+		pc.Client = client
 	}
 }
 
-func New(proxyIP string, request http.Request) *ProxyCoordinator {
-
+func (pc *ProxyCoordinator) AddLoadMaker(proxyUrl string, endpoint string, method string, body any) (*ProxyCoordinator, error) {
+	buf := &bytes.Buffer{}
+	err := json.NewEncoder(buf).Encode(body)
+	if err != nil {
+		return nil, fmt.Errorf("could not encode %v to json: %w", body, err)
+	}
+	req, err := http.NewRequest(method, proxyUrl+"/"+endpoint, buf)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating http request for proxy %s: %w", proxyUrl, err)
+	}
+	req.Header.Set(DelayHeaderParam, fmt.Sprintf("%di", pc.TargetTime.Sub(time.Now()).Milliseconds()))
+	lm := &LoadMaker{
+		ProxyUrl: proxyUrl,
+		Endpoint: endpoint,
+		Request:  req,
+	}
+	pc.LoadMakers = append(pc.LoadMakers, lm)
+	return pc, nil
 }
